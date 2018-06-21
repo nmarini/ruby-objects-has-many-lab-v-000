87004d5c81c040ebbdbce48f38f9dddb0f5a7318
Nicolas Marini <nmarini44@gmail.com>
2018-06-21 00:24:55 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,20 @@
 class Artist
   attr_accessor :name, :song
-
+@@song = []
   def initialize(name)
     @name = name
   end
-
+def songs 
+  @@songs
 end
+
+  def add_song(song)
+    song = Song.new(song)
+    @song = song 
+    song.artist.name = self 
+    @@songs << song  
+  end
+
+  
+
+end 
