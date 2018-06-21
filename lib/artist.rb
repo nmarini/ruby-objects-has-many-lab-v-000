c45d9d87346ff96b8225079f2f69dacd7e87c992
Nicolas Marini <nmarini44@gmail.com>
2018-06-21 00:49:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,15 +5,20 @@ class Artist
     @name = name
     @songs = []
   end
-def songs
-  @songs
-end
-
-  def add_song(song)
-  song.artist = self
-  @songs << song
+  def songs
+    @songs
   end
 
+  def add_song(song)
+    song.artist = self
+    @songs << song
+  end
+
+  def add_song_by_name(song_name)
+    song_name = Song.new(song_name)
+    song_name.artist = self 
+    @song << song_name
+    
 
 
 
